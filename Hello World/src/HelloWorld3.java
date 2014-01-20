1e18277ad68b8f1ffa07403b0b844268231c0b53
dconst02 <dconst02@TOOLS27.dcs.bbk.ac.uk>
2014-01-20 19:47:23 +0000

    committing no changes

---
+++ Hello World/src/HelloWorld3.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld3 {
+	
+	public static void main (String[] args)
+	{
+		System.out.println ("Good morning morning morning morning");
+		System.out.println ("Good afternoon");
+		System.out.println ("Good night");
+	}
+
+}
