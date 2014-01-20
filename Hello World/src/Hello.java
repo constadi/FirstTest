343a1e7fffb5b5c923d0879bd7e56c3f38d0fb41
constadi <diana.constable@hotmail.com>
2014-01-20 20:45:05 +0000

    Update Hello.java

--- Hello World/src/Hello.java
+++ Hello World/src/Hello.java
@@ -1,9 +1,8 @@
-
 public class Hello {
 	
 	public static void main (String[] args)
 	{
-		System.out.println ("Hello Wolrd Hello Hello Hello");
+		System.out.println ("Hello Wolrd Hello again");
 		System.out.println ("from Diana");
 	}
 
